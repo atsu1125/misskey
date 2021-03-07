4d5e9b7f330baa059ef192b3a32ffa40e6e4502e
mei23 <m@m544.net>
2021-03-08 00:52:13 +0900

    fix

--- src/server/api/endpoints/users/reaction-stats.ts
+++ src/server/api/endpoints/users/reaction-stats.ts
@@ -123,7 +123,7 @@ export default define(meta, async (ps, me) => {
 			$skip: ps.offset
 		},
 		{
-			$limit: ps.limit
+			$limit: ps.limit * 2
 		}
 	]) as Promise<ReactionStat[]>;
 
@@ -132,7 +132,6 @@ export default define(meta, async (ps, me) => {
 	const reactions = xs.map(x => {
 		return {
 			count: x.count,
-			raw: x._id,
 			reaction: decodeReaction(x._id)
 		}
 	});
@@ -140,17 +139,31 @@ export default define(meta, async (ps, me) => {
 	const reacteds = ys.map(x => {
 		return {
 			count: x.count,
-			raw: x._id,
 			reaction: decodeReaction(x._id)
 		}
 	});
+
+	// なんか被るので多めに取得して再集計
+	const n: Record<string, number> = {};
+	for (const r of reacteds) {
+		if (r.reaction === '__proto__') continue;
+		if (n[r.reaction]) {
+			n[r.reaction] += r.count;
+		} else {
+			n[r.reaction] = 0;
+		}
+	}
+
+	const reacteds2 = Object.keys(n)
+		.map(x => ({ reaction: x, count: n[x] }))
+		.sort((a, b) => a.count - b.count);
 
 	const reactionNames = unique(concat([xs.map(x => x._id), ys.map(x => x._id)]));
 	const emojis = await packEmojis([], null, reactionNames.map(x => decodeReaction(x)).map(x => x.replace(/:/g, '')));
 
 	const r = {
 		reactions,
-		reacteds,
+		reacteds: reacteds2,
 		emojis
 	}
 
