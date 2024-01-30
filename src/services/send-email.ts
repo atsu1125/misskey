d672f2e8e0000c6d3f18095a240da6964fa87f14
atsu1125 <atsuchan@atsuchan.page>
2024-01-30 20:09:13 +0900

    Fix: not to send email if disabled

--- src/services/send-email.ts
+++ src/services/send-email.ts
@@ -8,6 +8,8 @@ export const logger = new Logger('email');
 export async function sendEmail(to: string, subject: string, html: string, text: string) {
 	const meta = await fetchMeta();
 
+	if (!meta.enableEmail) return;
+
 	const emailSettingUrl = `${config.url}/i/settings/profile`;
 
 	const enableAuth = meta.smtpUser != null && meta.smtpUser !== '';
