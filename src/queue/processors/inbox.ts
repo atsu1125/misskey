2f2f636486792bf9545f5bfd3b689892e744b0bf
atsu1125 <atsuchan@atsuchan.page>
2025-05-23 23:46:31 +0900

    fix(backend): inbox skip activity id is not a string

--- src/queue/processors/inbox.ts
+++ src/queue/processors/inbox.ts
@@ -139,6 +139,8 @@ export const tryProcessInbox = async (data: InboxJobData, ctx?: ApContext): Prom
 		if (signerHost !== activityIdHost) {
 			return `skip: signerHost(${signerHost}) !== activity.id host(${activityIdHost}`;
 		}
+	} else {
+		return `skip: activity id is not a string`;
 	}
 
 	// Update stats
